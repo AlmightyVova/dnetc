2ff62f5d56bbf19143a89a76abae7755f039ab4e
Vladimir Legno <vovkolimur@ukr.net>
2020-11-06 20:52:01 +0200

    done

---
+++ hw4es/hw4es/DocumentReader.cs
@@ -0,0 +1,20 @@
+﻿namespace hw4es
+{
+    public class Manipulator
+    {
+        public AbstractHandler Handler { get; }
+
+        public Manipulator(string filename)
+        {
+            string extension = filename.Substring(filename.LastIndexOf('.'));
+            string title = filename.Substring(0, filename.Length - extension.Length);
+            Handler = extension switch
+            {
+                ".xml" => new XMLHandler(filename, title, extension),
+                ".txt" => new TXTHandler(filename, title, extension),
+                ".doc" => new DOCHandler(filename, title, extension),
+                _ => Handler
+            };
+        }
+    }
+}
